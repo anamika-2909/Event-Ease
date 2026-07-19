16af0c7008df4ad00b1fd09989f8c590291d1653
Anamika <makwanaanamika5@gmail.com>
2026-07-19 14:14:09 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Layout from "./Layout/layout";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
+import Services from "./pages/service";
 
 const App = () => {
   return (
