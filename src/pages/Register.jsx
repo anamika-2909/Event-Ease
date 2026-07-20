1f54c9da8dd5db6bd99a32d8e001794911322dbf
Anamika <makwanaanamika5@gmail.com>
2026-07-20 13:01:51 +0530

    Ok

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import axiosInstance from "../service/axiosInstance";
 
 const Register = () => {
