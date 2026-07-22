dc20e1f776ab5e5f04a274dbdbc19d6606df9e3d
Anamika <makwanaanamika5@gmail.com>
2026-07-22 11:20:03 +0530

    Done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,42 @@ import { Link } from "react-router-dom";
 import { FaEye, FaEnvelope, FaLock } from "react-icons/fa";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axiosInstance.post("/user/login", form);
+
+            if (res.data.success) {
+                localStorage.setItem("token", res.data.token);
+                localStorage.setItem("user", JSON.stringify(res.data.user));
+
+                alert("Login Successfully");
+
+                navigate("/");
+            } else {
+                alert(res.data.message);
+            }
+        } catch (err) {
+            alert(err.response?.data?.message || "Login Failed");
+        }
+    };
     return (
         <section className="login-section">
             <div className="container">
@@ -46,7 +82,7 @@ const Login = () => {
                                         <p className="text-muted mb-4">
                                             Login to your account
                                         </p>
-                                        <form>
+                                        <form onSubmit={handleSubmit}>
                                             <div className="mb-3">
                                                 <label>Email</label>
                                                 <div className="input-group">
@@ -56,7 +92,11 @@ const Login = () => {
                                                     <input
                                                         type="email"
                                                         className="form-control"
-                                                        placeholder="Enter Email" />
+                                                        placeholder="Enter Email"
+                                                        name="email"
+                                                        value={form.email}
+                                                        onChange={handleChange}
+                                                    />
                                                 </div>
                                             </div>
                                             {/* Password */}
@@ -75,12 +115,18 @@ const Login = () => {
                                                         type="password"
                                                         className="form-control"
                                                         placeholder="Enter Password"
+                                                        name="password"
+                                                        value={form.password}
+                                                        onChange={handleChange}
                                                     />
 
-                                                    <span className="input-group-text">
-                                                        <FaEye />
+                                                    <span
+                                                        className="input-group-text"
+                                                        style={{ cursor: "pointer" }}
+                                                        onClick={() => setShowPassword(!showPassword)}
+                                                    >
+                                                        {showPassword ? <FaEyeSlash /> : <FaEye />}
                                                     </span>
-
                                                 </div>
 
                                             </div>
@@ -113,7 +159,7 @@ const Login = () => {
 
                                             </div>
 
-                                            <button className="btn btn-primary w-100 py-2">
+                                            <button type="submit" className="btn btn-primary w-100 py-2">
                                                 Login
                                             </button>
 
